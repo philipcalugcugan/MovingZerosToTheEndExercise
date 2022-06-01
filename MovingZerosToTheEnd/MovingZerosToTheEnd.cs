27729f0f201d5ca1c4427efd48e63bb0606aef72
John Philip Calugcugan <jcalugcugan@webcreek.com>
2022-06-01 21:03:51 +0800

    Initial Commit

---
+++ MovingZerosToTheEnd/MovingZerosToTheEnd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovingZerosToTheEnd
+{
+    public class MovingZerosToTheEnd
+    {
+        public int[] MoveZeroes(int[] arr)
+        {
+            var nonZeroes = arr.ToList().Where(w => w != 0).ToList();
+            var zeroes = arr.ToList().Where(z => z == 0).ToList();
+            var newArrangedList = new List<int>();
+            newArrangedList.AddRange(nonZeroes);
+            newArrangedList.AddRange(zeroes);
+
+            return newArrangedList.ToArray();
+        }
+    }
+}
---
+++ MovingZerosToTheEnd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MovingZerosToTheEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            *  Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+            */
+
+            var movingZerosToTheEndHelper = new MovingZerosToTheEnd();
+            var answer = movingZerosToTheEndHelper.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
+
+            Console.ReadLine(); // Wait for press key before exit
+        }
+    }
+}
